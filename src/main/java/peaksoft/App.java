e1b279cc585e5ce056bff774c148a4ce9146f410
kalmurzauulu <alimbaev.988@gmail.com>
2022-03-14 20:40:59 +0300

    commit1

--- src/main/java/peaksoft/App.java
+++ src/main/java/peaksoft/App.java
@@ -7,8 +7,19 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+       HelloWorld bean1= applicationContext.getBean("helloworld",HelloWorld.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        System.out.println(bean==bean1);
+
+
+        Cat catBean = applicationContext.getBean("cat",Cat.class);
+        System.out.println(catBean);
+
+        Cat catBean1 = applicationContext.getBean("cat",Cat.class);
+        System.out.println(catBean1);
+        System.out.println(catBean==catBean1);
+
     }
 }
--- src/main/java/peaksoft/AppConfig.java
+++ src/main/java/peaksoft/AppConfig.java
@@ -2,6 +2,7 @@ package peaksoft;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
@@ -10,5 +11,11 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+    @Bean
+    @Scope("prototype")
+    public Cat cat(){
+        Cat cat = new Cat("alabay");
+        return cat;
     }
 }
